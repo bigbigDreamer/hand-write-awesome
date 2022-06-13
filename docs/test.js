a469b74131a672efcfd66096e4c911c9abb7551f
eric_wang <jadeface.scholar@gmail.com>
2022-06-14 00:03:57 +0800

    feat: add test.js

--- docs/test.js
+++ docs/test.js
@@ -1,33 +1,26 @@
-let totalPosts = 0;
-let ref = {
-    get current() {
-        console.log("Stack Watch Get")
-        return totalPosts;
-    },
+/**
+ * @desc 跳出 forEach 循环
+ */
+const arr = [1, 2, 3, 4, 5];
+const log = (...rest) => console.log.apply(this, rest);
+const logErr = (...rest) => console.error.apply(this, rest);
 
-    set current(value) {
-        totalPosts = value;
+// 传统 for 循环
+for (let i = 0; i<arr.length; i++) {
+    if(arr[i] === 2) {
+        log("item--for-loop", arr[i]);
+        break;
     }
 }
-function sleep(interval) {
-    return new Promise(resolve => {
-        setTimeout(resolve, interval);
-    })
-}
-async function getPosts(userId) {
-    const users = [{ id: 1, posts: 5 }, { id: 2, posts: 3 }];
-    const delay = Math.random() * 1000;
-    console.log("睡眠：", delay, "userId:", userId, "检索值：", users.find((user) => user.id === userId).posts)
-    await sleep(delay);
-    return users.find((user) => user.id === userId).posts;
-}
 
-async function addPosts(userId) {
-    console.log("Every Initial value:", "ref.current")
-    ref.current += await getPosts(userId);
+// forEach
+try {
+    arr.forEach(item => {
+        if(item === 2) {
+            log("item--for-loop", item);
+            throw new Error('item is 2');
+        }
+    });
+} catch (e) {
+    logErr(e.toString());
 }
-
-Promise.all([addPosts(1), addPosts(2)])
-    .then(() => {
-        console.log('Post count:', ref.current);
-    })
